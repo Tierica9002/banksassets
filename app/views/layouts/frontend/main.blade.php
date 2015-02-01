763c26d093f6f61563201a6d915173547ddf34a9
Emanuel Sandu <e.sandu@gmail.com>
2015-02-01 16:08:06 +0200

    First Emi commit.

--- app/views/layouts/frontend/main.blade.php
+++ app/views/layouts/frontend/main.blade.php
@@ -52,7 +52,7 @@
                                 <span class="icon-bar"></span>
                             </button>
                             <div class="navbar-brand nav" id="brand">
-                                <a href="index-google-map-fullscreen.html"><img src="{{asset('img/logo.png')}}" alt="brand"></a>
+                                <a href="#te-duce-la-homepage"><img src="{{asset('img/BA-Logo.png')}}" alt="brand"></a>
                             </div>
                         </div>
                         <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">

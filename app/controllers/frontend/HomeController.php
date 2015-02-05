0c02c2080969521089f9185a296c73de908b9eb6
Andrei Sorescu <and.sorescu@gmail.com>
2015-02-05 13:46:05 +0200

    Added base url in JS

--- app/controllers/frontend/HomeController.php
+++ app/controllers/frontend/HomeController.php
@@ -1,26 +1,38 @@
 <?php
 
 class HomeController extends BaseController {
+    /*
+      |--------------------------------------------------------------------------
+      | Default Home Controller
+      |--------------------------------------------------------------------------
+      |
+      | You may wish to use controllers instead of, or in addition to, Closure
+      | based routes. That's great! Here is an example controller method to
+      | get you started. To route to this controller, just add the route:
+      |
+      |	Route::get('/', 'HomeController@showWelcome');
+      |
+     */
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+    public function index() {
+        $context_data = [];
+        $context_data['body_class'] = 'page-homepage navigation-fixed-top page-slider horizontal-search';
+        
+        return View::make('frontend.index',array('context_data' => $context_data));
+    }
 
-	public function index() {
-		return View::make('frontend.index');
-	}
+    public function about() {
+        $context_data = [];
+        $context_data['body_class'] = 'page-sub-page page-about-us';
         
-        public function about() {
-                return View::make('frontend.about');
-        }
-        
+        return View::make('frontend.about',array('context_data' => $context_data));
+    }
+
+    public function contact() {
+        $context_data = [];
+        $context_data['body_class'] = 'page-sub-page page-contact';
+        $context_data['scripts'] = array('');        
+        return View::make('frontend.contact',array('context_data' => $context_data));   
+    }
+
 }

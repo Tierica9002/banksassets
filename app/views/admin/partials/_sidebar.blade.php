b41b3aad6b5f0e2ecead7dd9f0f05b990337ebba
Andrei Sorescu <and.sorescu@gmail.com>
2015-02-13 18:02:58 +0200

    Added menu properties

--- app/views/admin/partials/_sidebar.blade.php
+++ app/views/admin/partials/_sidebar.blade.php
@@ -1,57 +1,84 @@
 <!-- BEGIN SIDEBAR -->
-    <div class="page-sidebar" data-pages="sidebar">
-      <div id="appMenu" class="sidebar-overlay-slide from-top">
-      </div>
-        <!-- BEGIN SIDEBAR HEADER -->
-        <div class="sidebar-header">
-            <img src="{{asset('admin/img/ba-logo-white-long.png')}}" alt="logo" class="brand" data-src="{{asset('admin/img/ba-logo-white-long.png')}}" data-src-retina="{{asset('admin/img/ba-logo-white-long.png')}}" width="108" height="25">
-           <div class="sidebar-header-controls">
+<div class="page-sidebar" data-pages="sidebar">
+    <div id="appMenu" class="sidebar-overlay-slide from-top">
+    </div>
+    <!-- BEGIN SIDEBAR HEADER -->
+    <div class="sidebar-header">
+        <img src="{{asset('admin/img/ba-logo-white-long.png')}}" alt="logo" class="brand" data-src="{{asset('admin/img/ba-logo-white-long.png')}}" data-src-retina="{{asset('admin/img/ba-logo-white-long.png')}}" width="108" height="25">
+        <div class="sidebar-header-controls">
             <button data-pages-toggle="#appMenu" class="btn btn-xs sidebar-slide-toggle btn-link m-l-20" type="button"><i class="fa fa-angle-down fs-16"></i></button>
             <button data-toggle-pin="sidebar" class="btn btn-link visible-lg-inline" type="button"><i class="fa fs-12"></i></button>
-          </div>
         </div>
-        <!-- END SIDEBAR HEADER -->
-        <!-- BEGIN SIDEBAR MENU -->
-        <div class="sidebar-menu">
-            <ul class="menu-items">
-                <li class="m-t-30">                                        
-                    <a href="{{ URL::route('administrator.dashboard') }}" class="detailed">
-                        <span class="title">Home</span>                        
-                    </a>
-                    <span class="icon-thumbnail "><i class="pg-home"></i>
-                    </span>
-                </li>
-                <li class>                                        
-                    <a href="{{ URL::route('administrator.user.index') }}" class="detailed">
-                        <span class="title">Users</span>                        
-                    </a>
-                    <span class="icon-thumbnail "><i class="fa fa-user"></i>
-                    </span>
-                </li>           
-                <li class>                                        
-                    <a href="{{ URL::route('administrator.complex.index') }}" class="detailed">
-                        <span class="title">Complexes</span>                        
-                    </a>
-                    <span class="icon-thumbnail "><i class="fa fa-building"></i>
-                    </span>
-                </li>               
-                <li class>                                        
-                    <a href="{{ URL::route('administrator.messages') }}" class="detailed">
-                        <span class="title">Messages</span>                        
-                    </a>
-                    <span class="icon-thumbnail "><i class="pg-mail"></i>
-                    </span>
-                </li>               
-                <li class>                                        
-                    <a href="{{ URL::route('administrator.contact.index') }}" class="detailed">
-                        <span class="title">Contacts</span>                        
-                    </a>
-                    <span class="icon-thumbnail "><i class="fa fa-user"></i>
-                    </span>
-                </li>               
-            </ul>
-            <div class="clearfix"></div>
-        </div>
-        <!-- END SIDEBAR MENU -->
     </div>
-    <!-- END SIDEBAR -->
+    <!-- END SIDEBAR HEADER -->
+    <!-- BEGIN SIDEBAR MENU -->
+    <div class="sidebar-menu">
+        <ul class="menu-items">
+            <li class="m-t-30">                                        
+                <a href="{{ URL::route('administrator.dashboard') }}" class="detailed">
+                    <span class="title">Home</span>                        
+                </a>
+                <span class="icon-thumbnail "><i class="pg-home"></i>
+                </span>
+            </li>
+            <li class>                                        
+                <a href="{{ URL::route('administrator.user.index') }}" class="detailed">
+                    <span class="title">Users</span>                        
+                </a>
+                <span class="icon-thumbnail "><i class="fa fa-user"></i>
+                </span>
+            </li>           
+            <li class>                                        
+                <a href="{{ URL::route('administrator.complex.index') }}" class="detailed">
+                    <span class="title">Complexes</span>                        
+                </a>
+                <span class="icon-thumbnail "><i class="fa fa-building"></i>
+                </span>
+            </li>               
+            <li class>                                        
+                <a href="{{ URL::route('administrator.messages') }}" class="detailed">
+                    <span class="title">Messages</span>                        
+                </a>
+                <span class="icon-thumbnail "><i class="pg-mail"></i>
+                </span>
+            </li>               
+            <li class>                                        
+                <a href="{{ URL::route('administrator.contact.index') }}" class="detailed">
+                    <span class="title">Contacts</span>                        
+                </a>
+                <span class="icon-thumbnail "><i class="fa fa-user"></i>
+                </span>
+            </li> 
+            <li class="">
+                <a href="javascript:;"><span class="title">Assets</span>
+                    <span class=" arrow"></span></a>
+                <span class="icon-thumbnail">A</span>
+                <ul class="sub-menu">
+                    <li class="">
+                        <a href="color.html">Villas</a>
+                        <span class="icon-thumbnail">v</span>
+                    </li>
+                    <li class="">
+                        <a href="typography.html">Lands</a>
+                        <span class="icon-thumbnail">t</span>
+                    </li>
+                    <li class="">
+                        <a href="icons.html">Offices</a>
+                        <span class="icon-thumbnail">o</span>
+                    </li>
+                    <li class="">
+                        <a href="buttons.html">Industrials</a>
+                        <span class="icon-thumbnail">i</span>
+                    </li>
+                    <li class="">
+                        <a href="notifications.html">Residential</a>
+                        <span class="icon-thumbnail">r</span>
+                    </li>                    
+                </ul>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+    <!-- END SIDEBAR MENU -->
+</div>
+<!-- END SIDEBAR -->

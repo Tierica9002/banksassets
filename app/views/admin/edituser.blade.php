966f3688b6c9bde3e5b68967eb0be28c02f2b9f8
Emanuel Sandu <e.sandu@gmail.com>
2015-02-14 14:50:13 +0200

    Changed formatting of "Edit User" page.

--- app/views/admin/edituser.blade.php
+++ app/views/admin/edituser.blade.php
@@ -17,32 +17,38 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 ">
                     <!-- START PANEL -->
-                    <div class="panel panel-transparent">
+                    <div class="panel panel-default">
                         <div class="panel-body">
                             {{ Form::open(['route' => ['administrator.user.update', $user->id],'method' => 'put', 'role' => 'form', 'autocomplete' => 'off']) }}
                                 <div class="row clearfix">
-                                    <div class="col-sm-6">
+                                    <div class="col-sm-3 col-sm-offset-3">
                                         <div class="form-group form-group-default required">
                                             <label>First name</label>
-                                            <input type="text" class="form-control" name="first_name" value="{{{ $user->first_name }}}" required>
+                                            <input type="text" class="form-control" name="first_name" value="{{{ $user->first_name }}}" required="">
                                         </div>
                                     </div>
-                                    <div class="col-sm-6">
-                                        <div class="form-group form-group-default">
+                                    <div class="col-sm-3">
+                                        <div class="form-group form-group-default required">
                                             <label>Last name</label>
-                                            <input type="text" class="form-control" name="last_name" value="{{{ $user->last_name }}}" required>
+                                            <input type="text" class="form-control" name="last_name" value="{{{ $user->last_name }}}" required="">
                                         </div>
                                     </div>
                                 </div>                                                               
                                 <div class="row">
-                                    <div class="col-sm-12">
-                                        <div class="form-group form-group-default">
+                                    <div class="col-sm-6 col-sm-offset-3">
+                                        <div class="form-group form-group-default required">
                                             <label>Email</label>
-                                            <input type="email" class="form-control" name="email" placeholder="" value="{{{ $user->email }}}" required>
+                                            <input type="email" class="form-control" name="email" placeholder="" value="{{{ $user->email }}}" required="">
                                         </div>
                                     </div>
                                 </div>                                                                
                                 <div class="clearfix"></div>
+                                <div class="row">
+                                    <div class="col-sm-6 col-sm-offset-3">
+                                        <button class="btn btn-success" type="submit">Save</button>
+                                        <span>or </span><a href="#">Cancel</a>
+                                    </div>
+                                </div>
                                 <button class="btn btn-primary" type="submit">Edit user</button>
                                 {{ $errors->first() }}
                             {{Form::close()}}
